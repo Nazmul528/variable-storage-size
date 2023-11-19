adfac228079ca6656d41ea1a80864ba8832ef345
Nazmul 528 -60-N <151040875+Nazmul528@users.noreply.github.com>
2023-11-19 11:35:12 +0600

    Add files via upload

---
+++ variable storage size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{   char c;
+    int i ;
+    double d;
+    float f;
+    printf("size of char   :%d byte\n",sizeof(c));
+    printf("size of int    :%d byte\n",sizeof(i));
+    printf("size of floar  :%d byte\n",sizeof(f));
+    printf("size of double :%d byte\n",sizeof(d));
+    
+    return 0;
+}
